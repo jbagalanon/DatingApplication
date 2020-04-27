9f92d79d57d648a3b8bf4c72e59cfea607b51678
Joemar <jbagalanon@gmail.com>
2020-04-27 12:39:51 +0800

    Testing API - POSTMAN

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingAPI.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingAPI.Controllers
 {
@@ -10,19 +12,29 @@ namespace DatingAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task <IActionResult> GetValues ()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+        public async Task<IActionResult> GetValue(int id)
+       {
+           var value =await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+           return Ok(value);
+       }
 
         // POST api/values
         [HttpPost]
