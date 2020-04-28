4d943bbd41b5cc17287485cd86b685c82ae40add
Joemar <jbagalanon@gmail.com>
2020-04-28 23:17:54 +0800

    Generate HMACSHA512

--- Models/AuthRepository.cs
+++ Models/AuthRepository.cs
@@ -18,10 +18,43 @@ namespace DatingAPI.Models
         public async Task<User> Login(string username, string password)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, user.PasswordSalt, user.PasswordHash))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] PasswordSalt, byte[] PasswordHash)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(PasswordSalt))
+            {
+             
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] !=PasswordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
         {
+
+            //check this implementation
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
@@ -45,9 +78,16 @@ namespace DatingAPI.Models
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
