10acc3b10050ca98cc426f10c24254804760279b
Joemar <jbagalanon@gmail.com>
2020-05-01 23:20:11 +0800

    AuthController Mos

--- Startup.cs
+++ Startup.cs
@@ -41,8 +41,8 @@ namespace DatingAPI
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
-                            Configuration.GetSection("AppSettings:Token").Value)),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes
+                            (Configuration.GetSection("AppSettings:Token").Value)),
                         ValidateIssuer = false,
                         ValidateAudience = false
                     };
