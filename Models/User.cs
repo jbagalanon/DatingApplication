303d8d6c5487dc9eb4682a5ea8817c4059850ba0
Joemar <jbagalanon@gmail.com>
2020-07-23 17:39:40 +0800

    Added user detailed info

--- Models/User.cs
+++ Models/User.cs
@@ -9,12 +9,8 @@ namespace DatingAPI.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
-
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
@@ -24,8 +20,6 @@ namespace DatingAPI.Models
         public string LookingFor { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-
-
         public ICollection<Photo> Photos { get; set; }
 
 
