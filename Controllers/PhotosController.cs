71cf78ea999ca8a0044843dbba14247fb1e14ba6
Joemar <jbagalanon@gmail.com>
2020-08-03 02:38:42 +0800

    Added PhotoForCreationDto = object mapper

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingAPI.Data;
+using DatingAPI.Dtos;
 using DatingAPI.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
